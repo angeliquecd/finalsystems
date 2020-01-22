dab53baf424791bcf9c1bfc0fb71132a00c4871a
Lauren <lpehlivanian00@stuy.edu>
2020-01-22 02:36:24 -0500

    create works now

--- go.c
+++ go.c
@@ -64,6 +64,13 @@ static void handle_sig(int signo){
 
 int main(int argc, char *argsv[]){
   int shmd, q;
+  int library_created = 0;
+  //see if artists[0] has shmd in it to see if library was created.
+  shmd = shmget(KEY, TAB_SIZE, 0);
+  int * arts = (int *) shmat(shmd, 0, 0);
+  if (arts[0]) library_created = 1;
+  shmdt(arts); 
+
   int i=0;
   char s[100];
   char * sep;
@@ -143,6 +150,7 @@ if (strcmp(s,"PLAYLIST")==0){
   read(fd,buff,1000);
   strsep(&buff,"\n");//put in one of those loops from shell project that strseps and goes one by one
   command[1]=buff;
+  printf("buff: %s\n", buff);
   cpid=fork();
   if (cpid){//parent
     printf("%d in sigint",cpid);
@@ -200,6 +208,7 @@ if (strcmp(s,"POPULATE")==0){
   clear_library();
   initialize_table();
   populate_songs(i);
+  library_created = 1;
   // int c=0;
   // printf("artist array shmds: ");
   // while (artists[c]){
@@ -212,85 +221,98 @@ if (strcmp(s,"POPULATE")==0){
   //add songs?
 }
 if (strcmp(s,"BROWSE")==0){
-printf("Song library: \n");
-// struct song_node * test = shmat(artists[0], 0, 0);
-// printf("TEST: %s\n", test->artist);
-// printf("next: %d/n", test->next);
-// test = shmat(test->next, 0, 0);
-// printf("TEST2: %s \n", test->artist);
-// shmdt(test);
-print_library();
+  if (library_created == 1) {
+    printf("Song library: \n");
+    // struct song_node * test = shmat(artists[0], 0, 0);
+    // printf("TEST: %s\n", test->artist);
+    // printf("next: %d/n", test->next);
+    // test = shmat(test->next, 0, 0);
+    // printf("TEST2: %s \n", test->artist);
+    // shmdt(test);
+    print_library();
+  } else {
+    printf("You must first populate your library.\n");
+  }
 
 }
 if (strcmp(s,"CREATE")==0){
-  int num; //represents number of songs in library
-  int id; //song id user chooses to add to playlist
-  int status;
-  char * path;
-  char * input = malloc(10); //user input used throughout.
-  char * done = malloc(10); //is user done creating playlist ("y" / "n")
-  done = "n";
-  //makes a lil shell where you can write to playlists
-  //i think we agreed that playlists are text files w a list of song addresses
-  printf("Welcome to the playlist maker! Enter the title of your new playlist: ");
-  //get name of playlist
-  char name[100];
-  char * sep;
-  fgets(name,100,stdin);
-  sep = &name[0];
-  strsep(&sep,"\n");
-  printf("\tYour playlist: %s\n", name);
+  if (library_created == 1) {
+    int num; //represents number of songs in library
+    int id; //song id user chooses to add to playlist
+    int status;
+    char * path;
+    char * input = malloc(10); //user input used throughout.
+    char * done = malloc(10); //is user done creating playlist ("y" / "n")
+    //done = "n";
+    //makes a lil shell where you can write to playlists
+    //i think we agreed that playlists are text files w a list of song addresses
+    printf("Welcome to the playlist maker! Enter the title of your new playlist: ");
+    //get name of playlist
+    char name[100];
+    char * sep;
+    fgets(name,100,stdin);
+    sep = &name[0];
+    strsep(&sep,"\n");
+    printf("\tYour playlist: %s\n", name);
 
-  //create file for playlist
-  int fd = open(strcat(name, ".txt"), O_CREAT | O_RDWR, O_APPEND | 0644);
-  if (fd < 0) printf("errno %d error: %s\n", errno, strerror(errno));
+    //create file for playlist
+    int fd = open(strcat(name, ".txt"), O_CREAT | O_RDWR, O_APPEND | 0644);
+    if (fd < 0) printf("errno %d error: %s\n", errno, strerror(errno));
 
-  printf("Playlist '%s' started!\n", name);
-  //continuous loop for user to add songs until they say stop
-  while (strcmp(done, "n") == 0) {
-    num = print_library(); // num represents total songs
-    printf("Total # songs: %d\n", num);
-    printf("Enter the ID of a song to add: ");
-    if (fgets(input,10,stdin) == NULL) printf("error getting input: %s", strerror(errno));
-    // printf("You entered: %s\n", input);
-    input = strsep(&input,"\n");
-    printf("You entered: %s\n", input);
-    //convert string input to int id:
-    id = atoi(input);
-    printf("Adding id: %d\n", id);
+    printf("Playlist '%s' started!\n", name);
+    //continuous loop for user to add songs until they say stop
+    while (strcmp(done, "y") != 0) {
+      num = print_library(); // num represents total songs
+      printf("Total # songs: %d\n", num);
+      printf("Enter the ID of a song to add: ");
+      if (fgets(input,10,stdin) == NULL) printf("error getting input: %s", strerror(errno));
+      // printf("You entered: %s\n", input);
+      input = strsep(&input,"\n");
+      //printf("You entered: %s\n", input);
+      //convert string input to int id:
+      id = atoi(input);
+      //printf("Adding id: %d\n", id);
 
-    //user entered non-number
-    if (id == 0) {
-      printf("Please enter a number!\n");
+      //user entered non-number
+      if (id == 0) {
+        printf("Please enter a number!\n");
+      }
+      //id is not valid
+      else if (id > num || id <= 0) {
+        printf("Please enter an ID that is in range!\n");
+      }
+      //input is a valid id
+      else {
+        // sprintf(input, "%d", id);
+        // status = write(fd, input, sizeof(input));
+        path = getNthNode(id)->path; // <- doesn't work but eventually will be code to get the path based on id
+        strcat(path,"\n");
+        status = write(fd, path, sizeof(path));
+        if (status == 0) printf("errno %d error: %s\n", errno, strerror(errno));
+      }
+      printf("Are you done building the playlist? y/n ");
+      fgets(done,100,stdin);
+      done = strsep(&done,"\n");
+      //printf("your input: %s\n", done);
     }
-    //id is not valid
-    else if (id > num || id <= 0) {
-      printf("Please enter an ID that is in range!\n");
-    }
-    //input is a valid id
-    else {
-      // sprintf(input, "%d", id);
-      // status = write(fd, input, sizeof(input));
-      path = getNthNode(id)->path; // <- doesn't work but eventually will be code to get the path based on id
-      strcat(path,"\n");
-      status = write(fd, path, sizeof(path));
-      if (status == 0) printf("errno %d error: %s\n", errno, strerror(errno));
-    }
-    printf("Are you done building the playlist? y/n ");
-    fgets(done,100,stdin);
-    done = strsep(&done,"\n");
+  } else {
+    printf("You must first create the library.\n");
   }
 
 }
 if (strcmp(s,"DELETE")==0){
-  int a;
-  for ( a=0;a<i;a++){
-  shmd=shmget(KEY2+a,SEG_SIZE,0);
-  q=shmctl(shmd,IPC_RMID,0);
-}
-shmd=shmget(KEY,TAB_SIZE,0);
-//printf("%d",shmd);
-q=shmctl(shmd,IPC_RMID,0);
+//   int a;
+//   for ( a=0;a<i;a++){
+//   shmd=shmget(KEY2+a,SEG_SIZE,0);
+//   q=shmctl(shmd,IPC_RMID,0);
+// }
+// shmd=shmget(KEY,TAB_SIZE,0);
+// //printf("%d",shmd);
+// q=shmctl(shmd,IPC_RMID,0);
+
+  del_library(i);
+  library_created = 0;
+//clear_library();
 printf("Library deleted.\n");
 }
 
