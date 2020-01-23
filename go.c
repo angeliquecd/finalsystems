05aa7b4efbfd3bef231d6634daf7739614a2f742
Lauren <lpehlivanian00@stuy.edu>
2020-01-23 09:19:42 -0500

    fixing seg fault in play artist

--- go.c
+++ go.c
@@ -43,7 +43,6 @@ int initmem(){
   shmget(KEY,TAB_SIZE, IPC_CREAT | 0644);
   return i;
 }
-
 static void handle_sig(int signo){
   printf("Signal is: %d\n",signo);
   if (signo==SIGINT){
@@ -77,14 +76,14 @@ int main(int argc, char *argsv[]){
   char * sep;
   struct song_node *data;
 i=initmem();
-  printf("\nWelcome to the music center! How would you like to proceed?\n\n");
+  printf("\nWelcome to the music center! How would you like to proceed?\n\n\n");
     while (strcmp(s,"EXIT")!=0){
   printf("Type POPULATE to populate the library\n");//temporary button
   printf("Type PLAY to play music\n");
   printf("Type CREATE to create a new playlist\n");
   printf("Type BROWSE to browse the library\n");
       //type artist, song title, album
-  printf("Type DELETE to delete the library. Then exit and restart.\n");
+  printf("To delete your library, re-populate it\n");
   printf("Type EXIT to exit the program\n");
   printf("\nEnter your selection: ");
 
@@ -135,15 +134,27 @@ if (strcmp(s,"SONG")==0){
   strcat(fpath, sep);
 
   command[1] = fpath;
- printf("\n\nCURRRENTLY PLAYING: %s\n",command[1]);
+
+  // sep = &s[0];
+  // strsep(&sep,"\n");
+  // printf("%d",artistshared[a]);
+  // while(artistshared[a]){
+  // shmd=artistshared[a];
+  // while (shmd){
+  // if (strcmp(s,get_title(shmd))==0){
+  //   printf("In here.\n");
+  //   char * path=getPath(shmd);
+  //   strcat(songs,path);
+  //   printf("songs: %s",songs);
+ printf("\n\nCURRRENTLY PLAYING; %s\n",command[1]);
    cpid=fork();
    if (cpid){//parent
-  //   printf("%d in sigint",cpid);
+     printf("%d in sigint",cpid);
      signal(SIGINT,handle_sig);
      wait (&status);
    }
    else
-   { // printf("%d not in sigint",cpid);
+   {  printf("%d not in sigint",cpid);
      execvp("play",command);
    }
    shmdt(song);
@@ -176,7 +187,6 @@ if (strcmp(s,"PLAYLIST")==0){
   printf("Songs in playlist '%s':\n", s);
   while (buff) {
     token = strsep(&buff, "\n");
-  //  printf("Token: %s",token);
     //if token is a path name (sometimes spacing is weird)
     if (strlen(token) > 0) {
       fpaths[i] = token;
@@ -214,25 +224,32 @@ if (strcmp(s,"PLAYLIST")==0){
   //free(buff);
 }
 if (strcmp(s,"ARTIST")==0){
-  num = print_library();
+  int i;
+  char * path;
   printf("Enter the artist name: ");
   fgets(s,100,stdin);
   sep = &s[0];
   strsep(&sep,"\n");
-  a=0;
   //printf("%d",artistshared[a]);
 while(artistshared[a]){
-//   //  printf("%s vs. ",s);
+   printf("\t%s vs. %s\n",s, get_artist(artistshared[a]));
 //   printf("%s\n",get_artist(artistshared[a]));
  if (strcmp(s,get_artist(artistshared[a]))==0){
    shmd=artistshared[a];
   while(shmd){
-    strncpy(songs,"songs/",100);
-       char * path=getPath(shmd);
-       //resets song path
-//printf("path: %s",path);
+    //reset songs:
+    songs[0] = 's';
+    songs[1] = 'o';
+    songs[2] = 'n';
+    songs[3] = 'g';
+    songs[4] = 's';
+    songs[5] = '/';
+    songs[6] = 0;
+    printf("songs: %s\n", songs);
+    path=getPath(shmd);
+printf("path: %s",path);
   strcat(songs,path);
-  //printf("songs: %s",songs);
+  printf("songs: %s",songs);
     command[1]=songs;
     //printf("%s",command[1]);
       //printf("In here");
@@ -360,24 +377,23 @@ if (strcmp(s,"CREATE")==0){
   }
 
 }
- if (strcmp(s,"DELETE")==0){
-  int a;
- for ( a=0;a<i;a++){
-   shmd=shmget(KEY2+a,SEG_SIZE,0);
-   q=shmctl(shmd,IPC_RMID,0);
- }
-shmd=shmget(KEY,TAB_SIZE,0);
- printf("%d",shmd);
- q=shmctl(shmd,IPC_RMID,0);
+// if (strcmp(s,"DELETE")==0){
+// //   int a;
+// //   for ( a=0;a<i;a++){
+// //   shmd=shmget(KEY2+a,SEG_SIZE,0);
+// //   q=shmctl(shmd,IPC_RMID,0);
+// // }
+// // shmd=shmget(KEY,TAB_SIZE,0);
+// // //printf("%d",shmd);
+// // q=shmctl(shmd,IPC_RMID,0);
 //
 //   del_library(i);
 //   library_created = 0;
-// clear_library();
-printf("Library deleted.\n");
-}
+// //clear_library();
+// printf("Library deleted.\n");
+// }
 
 }
   shmdt(data);
   printf("\nExiting the program.\n");
-  exit(0);
 }
